144723e22d778d9723bc7a7556bacac9724c0c22
sk1119 <stamatios.kourkoutas19@imperial.ac.uk>
2020-01-21 12:30:37 +0000

    add2

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -31,13 +31,13 @@ public class QueryProcessor {
 
             String[] nums = temp[1].split(" ");
             int num1 = Integer.parseInt(nums[0]);
-            int num2 = Integer.parseInt(nums[0]);
+            int num2 = Integer.parseInt(nums[1]);
             System.out.println(num1);
             String x = "";
             if(num1>num2){
                 x = Integer.toString(num1);
             }
-            else if(num2>num1){
+            else {
                 x = Integer.toString(num2);
             }
             return x;
